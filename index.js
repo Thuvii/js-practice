6dcc0978541f31e5b23f89e249b54fda7e65bd79
Thuvii <buihhh321@gmail.com>
2024-10-18 17:11:07 -0400

    done range input

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var rowInput = document.getElementById('row')
 var rowValue = document.querySelector('.row-value')
+var columnInput = document.getElementById('column')
+var columnValue = document.querySelector('.column-value')
 
+
+columnValue.textContent = columnInput.value
 rowValue.textContent = rowInput.value
-rowInput.addEventListener("input",(event) =>{
-  rowValue.textContent =event.target.value;
-})
 
-
+function rangeInput(inputValue, outputValue){
+  inputValue.addEventListener("input",(event)=>{
+    outputValue.textContent = event.target.value;
+  })
+}
+rangeInput(rowInput, rowValue)
+rangeInput(columnInput, columnValue)
